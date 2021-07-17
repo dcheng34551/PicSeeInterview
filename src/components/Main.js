5a4c63b2ac1bfe539405353536e8254b23034a32
dcheng34551 <dcheng34551@gmail.com>
2021-07-17 12:53:30 +0800

    keep mopving on to finish the topic

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router';
 import queryString from 'query-string';
 
+// try to finish the topic
+
 const Main = (props) => {
 	const [url, setUrl] = useState('');
 	const [currentUrl, setCurrentUrl] = useState([]);
